73fd454d965a5235e5616e9d26d614f580ae763f
Sultan1901 <sultan.aqeel.elq.js@tuwaiq.edu.sa>
2022-08-16 22:49:22 +0300

    finish all

--- src/Pages/Nutrition.js
+++ src/Pages/Nutrition.js
@@ -113,7 +113,7 @@ const Nutrition = () => {
 
   const result = async () => {
     const data = await axios
-      .get(`https://dwgh.herokuapp.com/${mrr}`, {
+      .get(`https://dwgh.herokuapp.com/getPatient/:mrn/${mrr}`, {
         mrn: mrr,
       })
       .then((result) => {
